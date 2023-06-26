94612cdb50f209a36d2b50685f54b9d785523f28
ryry <resnonmotus@gmail.com>
2023-06-26 19:52:47 +0800

    Getting started with Terraform

---
+++ devops-starter/terraform/README.md
@@ -0,0 +1,27 @@
+## Terraform getting started
+
+A tool for managing infrastracture with configuration files.
+
+--- 
+
+### Setup
+
+- Install Tertaform
+
+    https://developer.hashicorp.com/terraform/downloads
+
+- Initialize Terraform
+
+    `terraform init`
+
+- Check execution plan
+
+    `terraform plan`
+
+-  Create the resource
+
+    `terraform init`
+
+- Stop the container
+
+    `terraform destroy`
---
+++ devops-starter/terraform/main.tf
@@ -0,0 +1,28 @@
+terraform {
+    required_providers {
+        docker = {
+            source      = "kreuzwerker/docker"
+            version     = "~> 3.0.1"
+        }
+    }
+}
+
+provider "docker" {
+  
+}
+
+# Pull the image
+resource "docker_image" "nginx" {
+  name          = "nginx"
+  keep_locally  = false # Don't keep the image after container is built
+}
+
+resource "docker_container" "nginx" {
+    image       = docker_image.nginx.image_id # Image which the container is based on
+    name        = "tutorial" # Container name
+
+    ports {
+        internal = 80
+        external = 8080
+    }
+}
